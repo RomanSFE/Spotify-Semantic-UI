e9302e449a6fb8e95345c1a244317a14e0d80bdc
Abdulla Al Roman <romannsu142@gmail.com>
2019-07-12 22:34:49 +0600

    All files upload

---
+++ Spotify_semantic-ui/js/main.js
@@ -0,0 +1,20 @@
+(function ($) {
+    "use strict";
+	
+	$(document).ready(function() {
+       
+      $('.ui.dropdown')
+		  .dropdown();
+
+	  $('.activating.element')
+		  .popup();
+
+	// music player st
+	
+	// music player ed
+	   
+	});
+	
+	
+	
+})(jQuery);
